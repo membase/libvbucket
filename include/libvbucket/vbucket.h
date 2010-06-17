91a5444bfddf55774d4bf5d6ecbc367a9a49b326
Dustin Sallings <dustin@spy.net>
2010-06-17 12:47:33 -0700

    Added doxy.
    Documentation may be read here:
    
                  http://labs.northscale.com/libvbucket/
    
    Change-Id: I08cc1f7c2500f2b6c59a723985b07af0464b2ccc
    Reviewed-on: http://review.northscale.com:8080/636
    Tested-by: Steve Yen <steve.yen@gmail.com>
    Reviewed-by: Steve Yen <steve.yen@gmail.com>

--- include/libvbucket/vbucket.h
+++ include/libvbucket/vbucket.h
@@ -14,6 +14,28 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
+
+/*! \mainpage libconflate
+ *
+ * \section intro_sec Introduction
+ *
+ * libvbucket helps you understand and make use of vbuckets for
+ * scaling kv services.
+ *
+ * \section docs_sec API Documentation
+ *
+ * Jump right into <a href="modules.html">the modules docs</a> to get started.
+ */
+
+/**
+ * VBucket Utility Library.
+ *
+ * \defgroup CD Creation and Destruction
+ * \defgroup cfg Config Access
+ * \defgroup vb VBucket Access
+ * \defgroup err Error handling
+ */
+
 #ifndef LIBVBUCKET_VBUCKET_H
 #define LIBVBUCKET_VBUCKET_H 1
 
@@ -24,53 +46,159 @@
 extern "C" {
 #endif
 
-struct vbucket_config_st;
+    struct vbucket_config_st;
 
-typedef struct vbucket_config_st* VBUCKET_CONFIG_HANDLE;
+    /**
+     * Opaque config representation.
+     */
+    typedef struct vbucket_config_st* VBUCKET_CONFIG_HANDLE;
 
-/* Creation and destruction */
-LIBVBUCKET_PUBLIC_API
-VBUCKET_CONFIG_HANDLE vbucket_config_parse_file(const char *filename);
-LIBVBUCKET_PUBLIC_API
-VBUCKET_CONFIG_HANDLE vbucket_config_parse_string(const char *data);
-LIBVBUCKET_PUBLIC_API
-void vbucket_config_destroy(VBUCKET_CONFIG_HANDLE h);
+    /**
+     * \addtogroup CD
+     *  @{
+     */
 
-/* Misc */
-LIBVBUCKET_PUBLIC_API
-const char *vbucket_get_error(void);
+    /**
+     * Create an instance of vbucket config from a file.
+     *
+     * @param filename the vbucket config to parse
+     */
+    LIBVBUCKET_PUBLIC_API
+    VBUCKET_CONFIG_HANDLE vbucket_config_parse_file(const char *filename);
 
-/* Config access */
-LIBVBUCKET_PUBLIC_API
-int vbucket_config_get_num_replicas(VBUCKET_CONFIG_HANDLE h);
-LIBVBUCKET_PUBLIC_API
-int vbucket_config_get_num_vbuckets(VBUCKET_CONFIG_HANDLE h);
-LIBVBUCKET_PUBLIC_API
-int vbucket_config_get_num_servers(VBUCKET_CONFIG_HANDLE h);
-LIBVBUCKET_PUBLIC_API
-const char *vbucket_config_get_server(VBUCKET_CONFIG_HANDLE h, int i);
+    /**
+     * Create an instance of vbucket config from a string.
+     *
+     * @param data a vbucket config string.
+     */
+    LIBVBUCKET_PUBLIC_API
+    VBUCKET_CONFIG_HANDLE vbucket_config_parse_string(const char *data);
 
-/* VBucket access */
-LIBVBUCKET_PUBLIC_API
-int vbucket_get_vbucket_by_key(VBUCKET_CONFIG_HANDLE h, const void *key, size_t nkey);
-LIBVBUCKET_PUBLIC_API
-int vbucket_get_master(VBUCKET_CONFIG_HANDLE h, int id);
-LIBVBUCKET_PUBLIC_API
-int vbucket_get_replica(VBUCKET_CONFIG_HANDLE h, int id, int n);
+    /**
+     * Destroy a vbucket config.
+     *
+     * @param h the vbucket config handle
+     */
+    LIBVBUCKET_PUBLIC_API
+    void vbucket_config_destroy(VBUCKET_CONFIG_HANDLE h);
 
-/**
- * Tell libvbucket it told you the wrong server ID.
- *
- * @param h the vbucket config handle.
- * @param vbucket the vbucket ID
- * @param wrongserver the incorrect server ID
- *
- * @return the correct server ID
- */
-LIBVBUCKET_PUBLIC_API
-int vbucket_found_incorrect_master(VBUCKET_CONFIG_HANDLE h,
-                                   int vbucket,
-                                   int wrongserver);
+    /**
+     * @}
+     */
+
+    /**
+     * \addtogroup err
+     *  @{
+     */
+
+    /**
+     * Get the most recent vbucket error.
+     *
+     * This is currently not threadsafe.
+     */
+    LIBVBUCKET_PUBLIC_API
+    const char *vbucket_get_error(void);
+
+    /**
+     * Tell libvbucket it told you the wrong server ID.
+     *
+     * This will cause libvbucket to do whatever is necessary to try
+     * to figure out a better answer.
+     *
+     * @param h the vbucket config handle.
+     * @param vbucket the vbucket ID
+     * @param wrongserver the incorrect server ID
+     *
+     * @return the correct server ID
+     */
+    LIBVBUCKET_PUBLIC_API
+    int vbucket_found_incorrect_master(VBUCKET_CONFIG_HANDLE h,
+                                       int vbucket,
+                                       int wrongserver);
+
+    /**
+     * @}
+     */
+
+    /**
+     * \addtogroup cfg
+     * @{
+     */
+
+    /**
+     * Get the number of replicas configured for each vbucket.
+     */
+    LIBVBUCKET_PUBLIC_API
+    int vbucket_config_get_num_replicas(VBUCKET_CONFIG_HANDLE h);
+
+    /**
+     * Get the total number of vbuckets;
+     */
+    LIBVBUCKET_PUBLIC_API
+    int vbucket_config_get_num_vbuckets(VBUCKET_CONFIG_HANDLE h);
+
+    /**
+     * Get the total number of known servers.
+     */
+    LIBVBUCKET_PUBLIC_API
+    int vbucket_config_get_num_servers(VBUCKET_CONFIG_HANDLE h);
+
+    /**
+     * Get the server at the given index.
+     *
+     * @return a string in the form of hostname:port
+     */
+    LIBVBUCKET_PUBLIC_API
+    const char *vbucket_config_get_server(VBUCKET_CONFIG_HANDLE h, int i);
+
+    /**
+     * @}
+     */
+
+    /**
+     * \addtogroup vb
+     * @{
+     */
+
+    /**
+     * Get the vbucket number for the given key.
+     *
+     * @param h the vbucket config
+     * @param key pointer to the beginning of the key
+     * @param nkey the size of the key
+     *
+     * @return a key
+     */
+    LIBVBUCKET_PUBLIC_API
+    int vbucket_get_vbucket_by_key(VBUCKET_CONFIG_HANDLE h,
+                                   const void *key, size_t nkey);
+
+    /**
+     * Get the master server for the given vbucket.
+     *
+     * @param h the vbucket config
+     * @param id the vbucket identifier
+     *
+     * @return the server index
+     */
+    LIBVBUCKET_PUBLIC_API
+    int vbucket_get_master(VBUCKET_CONFIG_HANDLE h, int id);
+
+    /**
+     * Get a given replica for a vbucket.
+     *
+     * @param h the vbucket config
+     * @param id the vbucket id
+     * @param n the replica number
+     *
+     * @return the server ID
+     */
+    LIBVBUCKET_PUBLIC_API
+    int vbucket_get_replica(VBUCKET_CONFIG_HANDLE h, int id, int n);
+
+    /**
+     * @}
+     */
 
 #ifdef __cplusplus
 }
