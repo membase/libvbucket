e4f86fc3a1545b5b6f22a6b5eb547af5fd0b803f
Sergey Avseyev <sergey.avseyev@gmail.com>
2012-06-08 20:17:50 +0300

    Fix tests on windows: testketama and testapp
    Fixes link errors like this:
    
    testketama.dobj : error LNK2019: unresolved external symbol _snprintf referenced in function _main
    testketama.exe : fatal error LNK1120: 1 unresolved externals
    NMAKE : fatal error U1077: '"C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\BIN\link.exe"' : return code '0x460'
    
    Change-Id: I4f73504d9717c44fb75bd0c3422f5d0f6be08a1b
    Reviewed-on: http://review.couchbase.org/16993
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

--- tests/testapp.c
+++ tests/testapp.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <strings.h>
 
 #include <libvbucket/vbucket.h>
 
--- tests/testketama.c
+++ tests/testketama.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 
 #include <libvbucket/vbucket.h>
 #include "src/hash.h"
